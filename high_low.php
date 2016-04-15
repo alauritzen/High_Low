7ce10ee0ebfd093d4e603bcda5785f1cfa98ff68
Alan <alaurit@gmail.com>
2016-04-15 14:37:30 -0500

    added counting of attempts

--- high_low.php
+++ high_low.php
@@ -2,15 +2,17 @@
 
 $randomNum=rand(1, 100);
 $guess=0;
+$attempts=0;
 
 while ($guess!=$randomNum) {
     fwrite (STDOUT, "Guess the number between 1 and 100!\n");
     $guess=fgets(STDIN);
+    $attempts++;
     if ($guess>$randomNum) {
-        fwrite (STDOUT, "It's lower than that!");
+        fwrite (STDOUT, "It's lower than that!\n");
     } else if ($guess<$randomNum) {
-        fwrite (STDOUT, "It's higher than that!");
+        fwrite (STDOUT, "It's higher than that!\n");
     } else {
-        fwrite (STDOUT, "WHAT?! INCONCEIVABLE!\n");
+        fwrite (STDOUT, "INCONCEIVABLE!\nYou guessed it in $attempts tries!\n");
     }
 }
