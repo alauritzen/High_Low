1fe86f1a8121358ee4477369e76620d33e45350b
Alan <alaurit@gmail.com>
2016-04-15 15:44:12 -0500

    added 'play again?' at end

--- high_low.php
+++ high_low.php
@@ -1,18 +1,28 @@
 <?php
 
-$randomNum=mt_rand(1, 100);
 $guess=0;
-$attempts=0;
+$play=true;
 
-fwrite (STDOUT, "Guess the number between 1 and 100!\n");
-while ($guess!=$randomNum) {
-    $guess=fgets(STDIN);
-    $attempts++;
-    if ($guess>$randomNum) {
-        fwrite (STDOUT, "It's lower than that!\n");
-    } else if ($guess<$randomNum) {
-        fwrite (STDOUT, "It's higher than that!\n");
+while ($play) {
+    $randomNum=mt_rand(1, 100);
+    $attempts=0;
+    fwrite (STDOUT, "Guess the number between 1 and 100!\n");
+    while ($guess!=$randomNum) {
+        $guess=fgets(STDIN);
+        $attempts++;
+        if ($guess>$randomNum) {
+            fwrite (STDOUT, "It's lower than that!\n");
+        } else if ($guess<$randomNum) {
+            fwrite (STDOUT, "It's higher than that!\n");
+        } else {
+            fwrite (STDOUT, "INCONCEIVABLE!\nYou guessed it in $attempts tries!\n");
+        }
+    }
+    fwrite (STDOUT, "Would you like to play again? (y/n)\n");
+    $play=trim(fgets(STDIN));
+    if ($play == "y") {
+        $play=true;
     } else {
-        fwrite (STDOUT, "INCONCEIVABLE!\nYou guessed it in $attempts tries!\n");
+        $play=false;
     }
 }
